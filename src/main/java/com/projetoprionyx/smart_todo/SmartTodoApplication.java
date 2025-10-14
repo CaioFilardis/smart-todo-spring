e44a9d4543e5f3ccb5a0fa7cf2d5fa670f159b04
Caio Filardis <caiosantosshake@hotmail.com>
2025-10-14 16:28:19 -0300

    Criando meu projeto Smart TO-DO.

---
+++ src/main/java/com/projetoprionyx/smart_todo/SmartTodoApplication.java
@@ -0,0 +1,13 @@
+package com.projetoprionyx.smart_todo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartTodoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartTodoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projetoprionyx/smart_todo/SmartTodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetoprionyx.smart_todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartTodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
