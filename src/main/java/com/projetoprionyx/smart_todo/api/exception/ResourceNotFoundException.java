7f0b6f6ad010314bccae7a4b72060d3e5fe5fefa
Caio Filardis <99473977+CaioFilardis@users.noreply.github.com>
2025-10-27 12:13:24 -0300

    Add ResourceNotFoundException class

---
+++ src/main/java/com/projetoprionyx/smart_todo/api/exception/ResourceNotFoundException.java
@@ -0,0 +1,8 @@
+package com.projetoprionyx.smart_todo.api.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+    
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
