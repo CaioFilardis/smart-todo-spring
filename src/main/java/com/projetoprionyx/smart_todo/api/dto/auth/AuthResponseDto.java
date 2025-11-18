79e2b3e4555d058dd76a22803feb141637bd6b35
Caio Filardis <caiosantosshake@hotmail.com>
2025-11-18 01:06:02 -0300

    Implementando melhorias.

--- src/main/java/com/projetoprionyx/smart_todo/api/dto/auth/AuthResponseDto.java
+++ src/main/java/com/projetoprionyx/smart_todo/api/dto/auth/AuthResponseDto.java
@@ -10,6 +10,7 @@ public class AuthResponseDto {
 
     private String accessToken;
     private String tokenType = "Bearer";
+    private String message;
 
     public AuthResponseDto(String accessToken) {
         this.accessToken = accessToken;
